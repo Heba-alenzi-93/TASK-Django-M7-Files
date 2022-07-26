16e94fd978b7ee432b3e737d772a411eae2aa2a6
apple <apple@apples-MacBook-Pro.local>
2022-07-26 18:12:16 +0300

    i did the task

--- burgerprince/settings.py
+++ burgerprince/settings.py
@@ -117,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "static/" # URL path
+STATIC_ROOT = BASE_DIR/"static" #directory path 
+MEDIA_URL = "media/" #URL path
+MEDIA_ROOT = BASE_DIR/"media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- burgerprince/urls.py
+++ burgerprince/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from branches import views
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
@@ -36,3 +38,8 @@ urlpatterns = [
         views.EmployeeRetrieveUpdateDestroyView.as_view(),
     ),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    #urlpatterns += media(settings.MEDIA_URL,document_root=settings.MEDIA)
